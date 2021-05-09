5e4c51539b30367a152cc7619a59b905a1384c73
최서연 <yeondublue@naver.com>
2021-05-09 15:15:16 +0900

    first commit

---
+++ CloneNetflexProject/CloneNetflexProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneNetflexProjectApp.swift
+//  CloneNetflexProject
+//
+//  Created by yeonBlue on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct CloneNetflexProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
